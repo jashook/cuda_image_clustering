602b31266fcbbe80ad9c40e69465191498d4b11e
jashook <jshook@smu.edu>
2014-05-03 19:59:48 -0500

    finished reading in a png file

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "lodepng.h"
 
@@ -45,18 +46,38 @@ char* check_arguments(int argc, char** args)
 
 }
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-int main(int argc, char** args)
+size_t hash(unsigned const char* string, size_t size)
 {
-   char* filename = check_arguments(argc, args);
+    int hash, i;
 
-   if (filename == NULL) return 1;
+    for (hash = i = 0; i < size; ++i)
+    {
+        hash += string[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
 
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+
+    return hash;
+
+}
+
+void hash_picture(unsigned const char* const image, size_t width, size_t height)
+{
+   size_t i;
+
+   for (i = 0; i < (height * width) / 160; ++i) printf("%d\n", hash(image + (160 * i), (size_t)40 * 40));
+
+}
+
+void read_png_file(const char* const filename)
+{
    unsigned error;
    unsigned char* image;
-   unsigned width, height;
+   size_t width, height;
    size_t i = 0;
 
    printf("Trying to open the file: %s\n", filename);
@@ -65,7 +86,26 @@ int main(int argc, char** args)
 
    if (error) printf("Error reading the image, %u: %s\n", error, lodepng_error_text(error));
 
-   else free(image);
+   else
+   {
+      hash_picture(image, height, width);
+
+      free(image);
+
+   }
+
+}
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+int main(int argc, char** args)
+{
+   char* filename = check_arguments(argc, args);
+
+   if (!filename) return 1;
+
+   read_png_file(filename);
 
    return 0;
 
